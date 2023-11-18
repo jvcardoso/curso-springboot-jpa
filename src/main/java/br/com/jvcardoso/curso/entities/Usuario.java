e55fd006dd2056c6feac1b6e72efe5efb1805021
jvcardoso <ventura.juliano@gmail.com.br>
2023-11-18 19:37:58 -0300

    Banco de dados H2, test profile, JPA

--- src/main/java/br/com/jvcardoso/curso/entities/Usuario.java
+++ src/main/java/br/com/jvcardoso/curso/entities/Usuario.java
@@ -3,9 +3,20 @@ package br.com.jvcardoso.curso.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_usuario")
 public class Usuario implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
